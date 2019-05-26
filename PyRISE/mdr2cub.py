1e6610539597060ab251bf9939336dc7290c53e3
Ross Beyer <rbeyer@rossbeyer.net>
2019-05-26 15:38:01 -0700

    Alan sent along a TIF file that was in I/F so I wanted to be able to deal with that.

--- PyRISE/mdr2cub.py
+++ PyRISE/mdr2cub.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Convert one of Alan's MDR files (in DN) to a cube file (in I/F)."""
+"""Convert one of Alan's MDR files (in DN or I/F) to a cube file (in I/F)."""
 
 # Copyright 2019, Ross A. Beyer (rbeyer@seti.org)
 #
@@ -17,14 +17,20 @@
 
 
 # This program is based on equations in make_IOF.pro, by Alan Delamere,
+# and on equations in the ISIS program hical.
 # 2019-05-22.
 
 import argparse
 import collections
 import logging
+import shutil
 import statistics
 import subprocess
+import sys
 from pathlib import Path
+from osgeo import gdal
+
+import pvl
 
 import PyRISE.hirise as hirise
 import PyRISE.util as util
@@ -33,7 +39,8 @@ import kalasiris as isis
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-o', '--output',  required=False, default='.iof.cub')
+    parser.add_argument('-o', '--output',  required=False, default='.mdr.iof.cub')
+    parser.add_argument('-e', '--edr',  required=True)
     parser.add_argument('mdr', metavar="MDR_file")
     parser.add_argument('-l', '--log',  required=False, default='WARNING',
                         help="The log level to show for this program, can be a "
@@ -48,39 +55,83 @@ def main():
 
     util.set_logging(args.log)
 
+    edr_path = Path(args.edr)
     mdr_path = Path(args.mdr)
 
-    h2i_path = mdr_path.with_suffix('.MDR.cub')
-    out_path = util.path_w_suffix(args.output, h2i_path)
+    to_del = isis.PathSet()
+
+    h2i_path = to_del.add(edr_path.with_suffix('.hi2isis.cub'))
+    out_path = util.path_w_suffix(args.output, edr_path)
 
     # The first thing Alan's program did was to crop the image down to only the
     # 'imaging' parts.  We're not doing that so the resultant file has a
-    # geometry similar # to what comes out of ISIS hical.
+    # geometry similar to what comes out of ISIS hical.
 
-    logging.info(isis.hi2isis(mdr_path, to=h2i_path).args)
+    # Convert the EDR to a cube file
+    logging.info(isis.hi2isis(edr_path, to=h2i_path).args)
 
-    fpa_t = statistics.mean([float(isis.getkey_k(h2i_path, 'Instrument',
-                                                 'FpaPositiveYTemperature')),
-                             float(isis.getkey_k(h2i_path, 'Instrument',
-                                                 'FpaNegativeYTemperature'))])
+    # Convert Alan's MDR to a cube file
+    mdr_cub_path = to_del.add(mdr_path.with_suffix('.mdr.cub'))
+    logging.info(f'Running gdal_translate {mdr_path} -of ISIS3 {mdr_cub_path}')
+    gdal.Translate(str(mdr_cub_path), str(mdr_path), format='ISIS3')
 
-    cid = hirise.get_CCDID_fromfile(h2i_path)
-    tdg = t_dep_gain(cid.ccdname, fpa_t)
-    suncorr = solar_correction()
-    sed = float(isis.getkey_k(h2i_path, 'Instrument', 'LineExposureDuration'))
-    zbin = 1  # Ideally, this comes from the hical conf file GainUnitConversionBinFactor
+    h2i_s = isis.getkey_k(h2i_path, 'Dimensions', 'Samples')
+    h2i_l = isis.getkey_k(h2i_path, 'Dimensions', 'Lines')
+    mdr_s = isis.getkey_k(mdr_cub_path, 'Dimensions', 'Samples')
+    mdr_l = isis.getkey_k(mdr_cub_path, 'Dimensions', 'Lines')
 
-    # The 'ziof' name is from the ISIS HiCal/GainUnitConversion.h, it is a
-    # divisor in the calibration equation.
-    ziof = (zbin * tdg * sed * 1e-6 * suncorr)
-    eqn = f"\(F1 / {ziof})"
-    try:
-        logging.info(isis.fx(f1=h2i_path, to=out_path, equ=eqn).args)
-    except subprocess.CalledProcessError as err:
-        print(err.stderr)
+    if h2i_s != mdr_s:
+        logging.critical(f'The number of samples in {h2i_path} ({h2i_s}) '
+                         f'and {mdr_cub_path} ({mdr_s}) are different. Exiting.')
+        sys.exit()
+
+    if h2i_l != mdr_l:
+        logging.critical(f'The number of lines in {h2i_path} ({h2i_l}) '
+                         f'and {mdr_cub_path} ({mdr_l}) are different. Exiting.')
+        sys.exit()
+
+    # Convert the EDR to the right bit type for post-HiCal Pipeline:
+    h2i_16b_p = to_del.add(h2i_path.with_suffix('.16bit.cub'))
+    logging.info(isis.bit2bit(h2i_path, to=h2i_16b_p, bit='16bit',
+                              clip='minmax', minval=0, maxval=1.5).args)
+
+    shutil.copyfile(h2i_16b_p, out_path)
+
+    # Is the MDR in DN or I/F?
+    maximum_pxl = float(pvl.loads(isis.stats(mdr_cub_path).stdout)['Results']['Maximum'])
+    if maximum_pxl < 1.5:
+        logging.info('MDR is already in I/F units.')
+        mdr_16b_p = to_del.add(mdr_cub_path.with_suffix('.16bit.cub'))
+        logging.info(isis.bit2bit(mdr_cub_path, to=mdr_16b_p, bit='16bit',
+                                  clip='minmax', minval=0, maxval=1.5).args)
+        logging.info(isis.handmos(mdr_16b_p, mosaic=out_path).args)
+    else:
+        logging.info('MDR is in DN units and will be converted to I/F.')
+        # This part isn't quite working yet.
+
+        fpa_t = statistics.mean([float(isis.getkey_k(h2i_16b_path, 'Instrument',
+                                                     'FpaPositiveYTemperature')),
+                                 float(isis.getkey_k(h2i_16b_path, 'Instrument',
+                                                     'FpaNegativeYTemperature'))])
+
+        cid = hirise.get_CCDID_fromfile(h2i_16b_path)
+        tdg = t_dep_gain(cid.ccdname, fpa_t)
+        suncorr = solar_correction()
+        sed = float(isis.getkey_k(h2i_16b_path, 'Instrument', 'LineExposureDuration'))
+        zbin = 1  # Ideally, this comes from the hical conf file GainUnitConversionBinFactor
+
+        # The 'ziof' name is from the ISIS HiCal/GainUnitConversion.h, it is a
+        # divisor in the calibration equation.
+        ziof = (zbin * tdg * sed * 1e-6 * suncorr)
+        eqn = f"\(F1 / {ziof})"
+
+        h2iiof_p = to_del.add(mdr_16b_p.with_suffix('.iof.cub'))
+        logging.info(isis.fx(f1=mdr_16b_p, to=mdriof_p, equ=eqn).args)
+
+        logging.info(isis.handmos(mdriof_p, mosaic=out_path).args)
 
     if not args.keep:
-        h2i_path.unlink()
+        to_del.unlink()
 
 
 def solar_correction() -> float:
@@ -95,6 +146,12 @@ def t_dep_gain(ccd: str, t: float) -> float:
        calculate the temperature dependent gain.'''
     # Equivalent to getTempDepGain() in ISIS HiCal/GainUnitConversion.h
     # But numbers all come from Alan.
+    # These equations are really g * (1 + t - baseT) * Q * absgainTDI
+    # where these variables are from the various hical .conf files.
+    #   g = FilterGainCorrection (DN/s) - Alan's numbers don't match
+    #   baseT = IoverFbasetemperature (deg C)  - Alan's numbers are the same
+    #   Q = QEpercentincreaseperC (1/deg C) - Alan's numbers are the same
+    #   absgainTDI = AbsGain_TDI128 (? units) - Alan's numbers don't match
     t_factor = 1 + (t - 18.9)
     if ccd == 'RED':
         return 157709797 * t_factor * 0.0005704 * 6.37688
