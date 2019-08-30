24d34ea6348b6d61a7b589d7c2bd111666a6d55b
Ross Beyer <rbeyer@rossbeyer.net>
2019-08-29 22:39:05 -0700

    Brought in the HiSlither functionality.

---
+++ PyRISE/HiSlither.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+"""HiSlither uses the output of HiJitReg to perform the registration of
+   color CCDs to red CCDs and output a color cube."""
+
+# Copyright 2019, Ross A. Beyer (rbeyer@seti.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# This program is based on HiColor version 1.99 2017/10/10
+# and on the Perl HiSlither program: ($Revision: 1.40 $ $Date: 2011/01/31 20:12:36 $)
+# by Guy McArthur
+# which is Copyright(C) 2007 Arizona Board of Regents, under the GNU GPL.
+#
+# Since that suite of software is under the GPL, none of it can be directly
+# incorporated in this program, since I wish to distribute this software
+# under the Apache 2 license.  Elements of this software (written in an entirely
+# different language) are based on that software but rewritten from scratch to
+# emulate functionality.
+
+import argparse
+import logging
+import math
+from datetime import datetime
+from pathlib import Path
+
+import kalasiris as isis
+import PyRISE.util as util
+import PyRISE.HiColorInit as hicolor
+import PyRISE.HiJitReg as hjr
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     parents=[util.parent_parser()])
+    parser.add_argument('cubes',
+                        metavar="RED balance.cub and color balance.precolor.cub files",
+                        nargs='+')
+
+    args = parser.parse_args()
+
+    util.set_logging(args.log)
+
+    cubes = list(map(hicolor.HiColorCube, args.cubes))
+    (red4, red5, ir10, ir11, bg12, bg13) = hicolor.separate_ccds(cubes)
+
+    if cube_check(red4, ir10, bg12):
+        HiSlither(red4, ir10, bg12, keep=args.keep)
+
+    if cube_check(red5, ir11, bg13):
+        HiSlither(red5, ir11, bg13, keep=args.keep)
+
+    return
+
+
+def cube_check(red: hicolor.HiColorCube, ir: hicolor.HiColorCube,
+               bg: hicolor.HiColorCube) -> bool:
+    if(red is None and
+       ir is None and
+       bg is None):
+        return False
+
+    if(red is None or bg is None):
+        have_ccds = [filter(lambda x: x is not None, [red, ir, bg])]
+        have_str = ''
+        if len(have_ccds) == 1:
+            have_str = str(have_ccds[0])
+        else:
+            have_str = '{} and {}'.format(*have_ccds)
+        logging.info('For a slither set, we need at least a RED and BG CCD.' +
+                     f'We only have: {have_str}')
+        return False
+
+    return True
+
+
+def HiSlither(red: hicolor.HiColorCube, ir: hicolor.HiColorCube,
+              bg: hicolor.HiColorCube, keep=False):
+    if red is None:
+        raise TypeError('Expected a RED HiColorCube, but got None.')
+    if bg is None:
+        raise TypeError('Expected a BG HiColorCube, but got None.')
+
+    logging.info('Beginning HiSlither for {}'.format(str(red)))
+
+    temp_token = datetime.now().strftime('HiSlither-%y%m%d%H%M%S')
+
+    if ir is None:
+        # If the IR band is missing, it is filled with a "dummy" band consisting
+        # entirely of null pixels. This band is created by masking the red band with
+        # itself. The dummy band allows for RGB (synthetic B) to be created by HiBeautify
+        # even if the IR band is missing.
+        logging.info('Missing IR, creating a null band.')
+        ir_slither_p = make_dummy_IR(red, bg)
+        ir = hicolor.HiColorCube(ir_slither_p)
+    else:
+        ir_slither_p = run_slither(ir)
+
+    bg_slither_p = run_slither(bg)
+
+    logging.info('Slither finished, merging stacks.')
+    logging.info('Creating {}-{}-{} mosaic'.format(ir.get_ccd(), red.get_ccd(),
+                                                   bg.get_ccd()))
+
+    stacked_path = red.path.parent / '{}_COLOR{}.cub'.format(red.get_obsid(), red.ccdnumber)
+    tmp_stack = stacked_path.with_suffix(f'.{temp_token}.cub')
+    logging.info(isis.hicubeit(ir=ir_slither_p, red=red.path,
+                               bg=bg_slither_p, to=tmp_stack).args)
+
+    trim_args = {'from': tmp_stack, 'to': stacked_path}
+    # trim 5 px in bin 1, 3 in bin 2, 2 in bin 4
+    trim = math.ceil(5 / red.bin)
+    if red.ccdnumber == '4':
+        trim_args['right'] = trim
+    else:
+        trim_args['left'] = trim
+    logging.info(isis.trim(**trim_args).args)
+
+    if not keep:
+            tmp_stack.unlink()
+
+    return stacked_path
+
+
+def get_slither_path(cube):
+    return cube.path.parent / '{}.slither.cub'.format(str(cube))
+
+
+def run_slither(cube):
+    cnet_path = hjr.JitterCube.get_cnet_path(cube)
+    s = get_slither_path(cube)
+    t = s.with_suffix('.txt')
+    logging.info(isis.slither(cube.path, to=s, control=cnet_path, results=t,
+                              dir='REVERSE', spline='CUBIC', interp='BILIN').args)
+    return(s)
+
+
+def make_dummy_IR(red, bg):
+        bg_slither_path = get_slither_path(bg)
+        ir_name = bg_slither_path.name.replace(bg.get_ccd(),
+                                               'IR' + str(int(bg.ccdnumber) - 2))
+        ir_path = bg_slither_path.parent / ir_name
+
+        if ir_path.exists():
+            raise FileExistsError("{} exists ".format(str(ir_path)) +
+                                  "and we don't want to overwrite it.")
+
+        ir_ccd = 'IR' + str(int(red.ccdnumber) + 6)
+
+        logging.info(isis.mask(red, mask=red, to=ir_path, preserve='outside').args)
+        logging.info(isis.editlab(ir_path, options='modkey',
+                                  grpname='Instrument',
+                                  keyword='CpmmNumber',
+                                  value=int(red.ccdnumber) + 2).args)
+        logging.info(isis.editlab(ir_path, options='modkey',
+                                  grpname='Instrument',
+                                  keyword='CcdId', value=ir_ccd).args)
+        logging.info(isis.editlab(ir_path, options='modkey',
+                                  grpname='Archive',
+                                  keyword='ProductID',
+                                  value='{}_{}'.format(red.get_obsid(), ir_ccd)).args)
+        logging.info(isis.editlab(ir_path, options='modkey',
+                                  grpname='BandBin',
+                                  keyword='Name',
+                                  value='NearInfrared').args)
+        logging.info(isis.editlab(ir_path, options='modkey',
+                                  grpname='BandBin',
+                                  keyword='Center',
+                                  value=900,
+                                  units='NANOMETERS').args)
+        logging.info(isis.editlab(ir_path, options='modkey',
+                                  grpname='BandBin',
+                                  keyword='Width',
+                                  value=200,
+                                  units='NANOMETERS').args)
+        return ir_path
---
+++ tests/test_HiSlither.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""This module has tests for the HiRISE HiJitReg functions."""
+
+# Copyright 2019, Ross A. Beyer (rbeyer@seti.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# import contextlib
+import unittest
+from unittest.mock import patch
+from pathlib import Path
+
+import PyRISE.HiColorInit as hci
+import PyRISE.HiSlither as sli
+
+
+def getkey(cube, group, key):
+            values = {'ProductId': None,
+                      'Summing': 2,
+                      'Lines': 1024,
+                      'Samples': 1024,
+                      'TDI': 64,
+                      'StitchedProductIds': ['PSP_010502_2090_RED4_0',
+                                             'PSP_010502_2090_RED4_1']}
+            return values[key]
+
+
+class TestHiSlither(unittest.TestCase):
+
+    @patch('PyRISE.HiColorInit.isis.getkey_k', side_effect=getkey)
+    def test_cube_check(self, m_getkey):
+        r = hci.HiColorCube('dummy/PSP_010502_2090_RED5.HiStitch.balance.cub')
+        i = hci.HiColorCube('dummy/PSP_010502_2090_IR11.HiStitch.balance.precolor.cub')
+        b = hci.HiColorCube('dummy/PSP_010502_2090_BG13.HiStitch.balance.precolor.cub')
+        self.assertTrue(sli.cube_check(r, i, b))
+        self.assertTrue(sli.cube_check(r, None, b))
+        self.assertFalse(sli.cube_check(None, None, None))
+        self.assertFalse(sli.cube_check(None, i, b))
+        self.assertFalse(sli.cube_check(r, i, None))
+
+    @patch('PyRISE.HiColorInit.isis.getkey_k', side_effect=getkey)
+    def test_get_slither_path(self, m_getkey):
+        c = hci.HiColorCube('dummy/PSP_010502_2090_IR10.HiStitch.balance.precolor.cub')
+        self.assertEqual(sli.get_slither_path(c),
+                         Path('dummy/PSP_010502_2090_IR10.slither.cub'))
+
+    @patch('PyRISE.HiSlither.isis.editlab')
+    @patch('PyRISE.HiSlither.isis.mask')
+    @patch('PyRISE.HiColorInit.isis.getkey_k', side_effect=getkey)
+    def test_make_dummy_IR(self, m_getkey, m_mask, m_editlab):
+        r = hci.HiColorCube('dummy/PSP_010502_2090_RED4.HiStitch.balance.cub')
+        b = hci.HiColorCube('dummy/PSP_010502_2090_BG12.HiStitch.balance.precolor.cub')
+        self.assertEqual(sli.make_dummy_IR(r, b),
+                         Path('dummy/PSP_010502_2090_IR10.slither.cub'))
+
+    @patch('PyRISE.HiSlither.isis.slither')
+    @patch('PyRISE.HiColorInit.isis.getkey_k', side_effect=getkey)
+    def test_run_slither(self, m_getkey, m_slither):
+        c = hci.HiColorCube('dummy/PSP_010502_2090_IR10.HiStitch.balance.precolor.cub')
+        sli.run_slither(c)
+        m_slither.called_once()
+
+    @patch('PyRISE.HiSlither.isis.trim')
+    @patch('PyRISE.HiSlither.isis.hicubeit')
+    @patch('PyRISE.HiSlither.run_slither')
+    @patch('PyRISE.HiColorInit.isis.getkey_k', side_effect=getkey)
+    def test_HiSlither(self, m_getkey, m_sli, m_hicubeit, m_trim):
+        r = hci.HiColorCube('dummy/PSP_010502_2090_RED5.HiStitch.balance.cub')
+        i = hci.HiColorCube('dummy/PSP_010502_2090_IR11.HiStitch.balance.precolor.cub')
+        b = hci.HiColorCube('dummy/PSP_010502_2090_BG13.HiStitch.balance.precolor.cub')
+        self.assertEqual(sli.HiSlither(r, i, b, keep=True),
+                         Path('dummy/PSP_010502_2090_COLOR5.cub'))
+        _, trim_args = m_trim.call_args
+        self.assertEqual(trim_args['left'], 3)
