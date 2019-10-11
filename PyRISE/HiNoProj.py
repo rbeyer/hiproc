0028dc5aaab8be9d0e8fc8c07817cf38be416a85
Ross Beyer <rbeyer@rossbeyer.net>
2019-10-10 19:58:48 -0700

    Added HiNoProj

---
+++ PyRISE/HiNoProj.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+"""Resamples images from individual CCDs to ideal camera geometry and
+mosaicks them into a single cube."""
+
+# Copyright 2019, Ross A. Beyer (rbeyer@seti.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# This program is based on HiPrecision version 1.20 2017/07/19
+# and on the Perl HiNoProj program: ($Revision: 1.9 $ $Date: 2017/08/30 21:26:39 $)
+# by Audrie Fennema
+# which is Copyright(C) 2008 Arizona Board of Regents, under the GNU GPL.
+#
+# Since that suite of software is under the GPL, none of it can be directly
+# incorporated in this program, since I wish to distribute this software
+# under the Apache 2 license.  Elements of this software (written in an entirely
+# different language) are based on that software but rewritten from scratch to
+# emulate functionality.
+
+import argparse
+import itertools
+import logging
+import os
+import re
+import shutil
+import sys
+from datetime import datetime
+from pathlib import Path
+
+import pvl
+
+import kalasiris as isis
+import PyRISE.hirise as hirise
+import PyRISE.util as util
+import PyRISE.HiColorInit as hci
+import PyRISE.HiColorNorm as hcn
+
+
+class Cube(hci.HiColorCube):
+
+    def __init__(self, pathlike):
+        super().__init__(pathlike)
+        self.noproj_path = None
+        self.line_offset = 0
+        self.samp_offset = 0
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     parents=[util.parent_parser()])
+    parser.add_argument('-o', '--output', required=False, default='_RED.NOPROJ.cub')
+    parser.add_argument('-c', '--conf',   required=False,
+                        default=Path(__file__).resolve().parent.parent /
+                        'resources' / 'HiNoProj.conf')
+    parser.add_argument('-b', '--base_ccd_number', required=False, default=5)
+    parser.add_argument('cubes', metavar="balance.cub-files", nargs='+')
+
+    args = parser.parse_args()
+
+    util.set_logging(args.log)
+
+    cubes = list(map(Cube, args.cubes))
+    cubes.sort()
+
+    if not all(c.ccdname == 'RED' for c in cubes):
+        logging.critical('Not all of the input files are RED CCD files.')
+        sys.exit()
+
+    sequences = list()
+    for k, g in itertools.groupby((int(c.ccdnumber) for c in cubes),
+                                  lambda x, c=itertools.count(): next(c) - x):
+        sequences.append(list(g))
+
+    if len(sequences) != 1:
+        logging.critical('The given cubes are not a single run of sequential '
+                         'HiRISE CCDs, instead there are '
+                         '{} groups with these CCD numbers: {}.'.format(len(sequences),
+                                                                        sequences))
+        sys.exit()
+
+    base_ccd = list(filter(lambda x: x.ccdnumber == str(args.base_ccd_number),
+                           cubes))
+    if len(base_ccd) != 1:
+        logging.critical(f'The base ccd, number {args.base_ccd_number}, is not '
+                         'one of the given cubes.')
+        sys.exit()
+
+    conf = pvl.load(str(args.conf))
+    conf_check(conf)
+
+    outcub_path = hcn.set_outpath(args.output, cubes)
+
+    import subprocess
+    try:
+        HiNoProj(cubes, base_ccd[0], outcub_path, conf['HiNoProj'], keep=args.keep)
+    except subprocess.CalledProcessError as err:
+        print('Had an ISIS error:')
+        print(err.cmd)
+        print(err.stdout)
+        print(err.stderr)
+        raise err
+    return
+
+
+def conf_check(conf: dict) -> None:
+    '''Various checks on parameters in the configuration.'''
+
+    t = 'Shape'
+    util.conf_check_strings(t, ('ELLIPSOID', 'SYSTEM', 'USER'), conf['HiNoProj'][t])
+
+    return
+
+
+def is_polar(cubes, pole_tolerance: float, temp_token: str) -> bool:
+
+    abs_lats = list()
+    for c in cubes:
+        temp_p = c.path.with_suffix(f'.{temp_token}.spice.cub')
+        shutil.copyfile(c.path, temp_p)
+
+        logging.info(isis.spiceinit(temp_p).args)
+
+        cam_cp = isis.camrange(temp_p)
+        logging.info(cam_cp.args)
+
+        cpvl = pvl.loads(cam_cp.stdout)
+
+        abs_lats.append(abs(float(cpvl['UniversalGroundRange']['MinimumLatitude'])))
+        abs_lats.append(abs(float(cpvl['UniversalGroundRange']['MaximumLatitude'])))
+
+        temp_p.unlink()
+
+    if max(abs_lats) > float(pole_tolerance):
+        return True
+    else:
+        return False
+
+
+def handmos_side(cubes, base_cube, out_p: os.PathLike, left=True):
+    '''Runs handmos to add cubes which are to one side or the other
+       of the *base_cube*.'''
+    # handmos left side
+    side = 1
+    priority = 'beneath'
+    if left:
+        side = -1
+        priority = 'ontop'
+    ssm = 1
+    slm = 1
+    for c in cubes[(cubes.index(base_cube) + side)::side]:
+        slm -= side * round(c.line_offset)
+        ssm -= side * round(c.samp_offset)
+        logging.info(isis.handmos(c.noproj_path, mosaic=out_p, priority=priority,
+                                  outline=slm, outsample=ssm, outband=1).args)
+    return
+
+
+def HiNoProj(cubes: list, base_cube, outcub_path: os.PathLike, conf: dict, keep=False):
+
+    temp_token = datetime.now().strftime('HiNoProj-%y%m%d%H%M%S')
+    to_del = isis.PathSet()
+    out_p = Path(outcub_path)
+
+    polar = False
+    if conf['Shape'] == 'USER':
+        polar = is_polar(cubes, conf['Pole_Tolerance'], temp_token)
+
+    for c in cubes:
+
+        temp_p = to_del.add(c.path.with_suffix(f'.{temp_token}.spiced.cub'))
+        shutil.copyfile(c.path, temp_p)
+
+        spiceinit_args = {'from': temp_p, 'shape': conf['Shape'],
+                          'cksmithed': True, 'spksmithed': True,
+                          'spkrecon': True, 'spkpredicted': False}
+
+        if conf['Shape'] == 'USER':
+            if polar:
+                spiceinit_args['model'] = conf['Polar_Shape_Model_Path']
+            else:
+                spiceinit_args['model'] = conf['Shape_Model_Path']
+
+        logging.info(isis.spiceinit(**spiceinit_args).args)
+
+        logging.info(isis.spicefit(temp_p).args)
+
+        c.noproj_path = to_del.add(c.path.with_suffix(f'.{temp_token}.noproj.cub'))
+        c.path = temp_p
+
+    for c in cubes:
+        logging.info(isis.noproj(c.path, match=base_cube.path, to=c.noproj_path,
+                                 source='frommatch').args)
+
+    # Run hijitreg on adjacent noproj'ed ccds to get average line/sample offset
+    for i, c in enumerate(cubes[:-1]):
+        pair = '{}-{}'.format(cubes[i].get_ccd(), cubes[i + 1].get_ccd())
+        flat_p = to_del.add(c.path.with_suffix(f'.{temp_token}.{pair}.flat.tab'))
+
+        logging.info(isis.hijitreg(cubes[i].noproj_path,
+                                   match=cubes[i + 1].noproj_path,
+                                   flat=flat_p).args)
+
+        with open(flat_p, 'r') as f:
+            flat = f.read()
+
+            match = re.search(r'#\s+Average Line Offset:\s+(\S+)', flat)
+            avg_line_offset = float(match.group(1))
+
+            match = re.search(r'#\s+Average Sample Offset:\s+(\S+)', flat)
+            avg_samp_offset = float(match.group(1))
+
+        j = i
+        if int(c.ccdnumber) >= int(base_cube.ccdnumber):
+            j = i + 1
+
+        cubes[j].line_offset = avg_line_offset
+        cubes[j].samp_offset = avg_samp_offset
+
+    # Mosaic noproj'ed ccds using average line/sample offset
+    shutil.copyfile(base_cube.noproj_path, out_p)
+    logging.info('Original Perl hard codes this file copy from RED5, even if '
+                 'another cube is selected as the base_ccd.')
+
+    handmos_side(cubes, base_cube, out_p, left=True)
+    handmos_side(cubes, base_cube, out_p, left=False)
+
+    logging.info(isis.editlab(out_p, option='addkey', grpname='Instrument',
+                              keyword='ImageJitterCorrected', value=0).args)
+    logging.info(isis.editlab(out_p, option='modkey', grpname='Archive',
+                              keyword='ProductId',
+                              value='{}_{}'.format(str(cubes[0].get_obsid()),
+                                                   cubes[0].ccdname)).args)
+    logging.info(isis.editlab(out_p, option='modkey', grpname='Instrument',
+                              keyword='MatchedCube',
+                              value=str(base_cube)).args)
+
+    # Fix ck kernel in InstrumentPointing in RED label
+    # This doesn't seem to be needed, maybe it was HiROC-specific.
+
+    #  Add SourceProductIds to Archive group in label
+    logging.info('Original Perl just assumes that both channels are included '
+                 'in the balance cube.')
+    source_ids = list()
+    for c in cubes:
+        source_ids.append(isis.getkey_k(c.path, 'Instrument', 'StitchedProductIds'))
+
+    logging.info(isis.editlab(out_p, option='ADDKEY', grpname='Archive',
+                              keyword='SourceProductId',
+                              value='({})'.format(', '.join(source_ids))).args)
+
+    if not keep:
+        to_del.unlink()
+
+    return
---
+++ tests/test_HiNoProj.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+"""This module has tests for the HiRISE HiccdStitch functions."""
+
+# Copyright 2019, Ross A. Beyer (rbeyer@seti.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# import contextlib
+import unittest
+from pathlib import Path
+from unittest.mock import call
+from unittest.mock import mock_open
+from unittest.mock import patch
+
+import pvl
+
+# import PyRISE.hirise as hirise
+import PyRISE.HiNoProj as hnp
+
+conf_path = Path('resources') / 'HiNoProj.conf'
+
+
+def getkey(cube, group, key):
+    values = {'ObservationId': 'PSP_010502_2090',
+              'ProductId': None,
+              'Summing': 2,
+              'Lines': 1024,
+              'Samples': 1024,
+              'Bands': 3,
+              'TDI': 64,
+              'Center': '(900, 700, 500) <NANOMETERS>',
+              'SourceProductId': ('PSP_010502_2090_RED4_0, PSP_010502_2090_RED4_1'),
+              'StitchedProductIds': ('PSP_010502_2090_RED4_0, PSP_010502_2090_RED4_1')}
+    return values[key]
+
+
+class TestHiNoProjCube(unittest.TestCase):
+
+    @patch('PyRISE.HiNoProj.isis.getkey_k', side_effect=getkey)
+    def test_init(self, mock_getkey):
+        c = hnp.Cube('dummy/PSP_010502_2090_RED5_0')
+        self.assertIsNone(c.noproj_path)
+
+
+class TestConf(unittest.TestCase):
+
+    def test_conf_check(self):
+        c = pvl.load(str(conf_path))
+        self.assertIsNone(hnp.conf_check(c))
+
+
+class TestHiNoProj(unittest.TestCase):
+
+    @patch('PyRISE.HiNoProj.Path.with_suffix')
+    @patch('PyRISE.HiNoProj.pvl.loads',
+           return_value={'UniversalGroundRange': {'MinimumLatitude': -66,
+                                                  'MaximumLatitude': 67}})
+    @patch('PyRISE.HiNoProj.isis.camrange')
+    @patch('PyRISE.HiNoProj.isis.spiceinit')
+    @patch('PyRISE.HiNoProj.shutil.copyfile')
+    @patch('PyRISE.HiNoProj.isis.getkey_k', side_effect=getkey)
+    def test_is_polar(self, mock_getkey, m_copy, m_spice, m_cam, m_pvl, m_path):
+        c1 = hnp.Cube('dummy/PSP_010502_2090_RED4.HiStitch.balance.cub')
+        c2 = hnp.Cube('dummy/PSP_010502_2090_RED3.HiStitch.balance.cub')
+
+        self.assertFalse(hnp.is_polar([c1, c2], 87, 'tt'))
+        self.assertTrue(hnp.is_polar([c1, c2], 60, 'tt'))
+
+    @patch('PyRISE.HiNoProj.isis.handmos')
+    @patch('PyRISE.HiNoProj.isis.getkey_k', side_effect=getkey)
+    def test_handmos_side(self, mock_getkey, m_hand):
+        c2 = hnp.Cube('dummy/PSP_010502_2090_RED2.HiStitch.balance.cub')
+        c3 = hnp.Cube('dummy/PSP_010502_2090_RED3.HiStitch.balance.cub')
+        c4 = hnp.Cube('dummy/PSP_010502_2090_RED4.HiStitch.balance.cub')
+        c5 = hnp.Cube('dummy/PSP_010502_2090_RED5.HiStitch.balance.cub')
+        c6 = hnp.Cube('dummy/PSP_010502_2090_RED6.HiStitch.balance.cub')
+
+        cubes = (c2, c3, c4, c5, c6)
+
+        for i, c in enumerate(cubes):
+            cubes[i].noproj_path = c.path
+            cubes[i].line_offset = 10
+            cubes[i].samp_offset = 10
+
+        hnp.handmos_side(cubes, c4, 'dummy/mosaic.cub', True)
+        hnp.handmos_side(cubes, c4, 'dummy/mosaic.cub', False)
+        self.assertEqual(m_hand.call_args_list,
+                         [call(c3.path, mosaic='dummy/mosaic.cub',
+                               outband=1, outline=11, outsample=11,
+                               priority='ontop'),
+                          call(c2.path, mosaic='dummy/mosaic.cub',
+                               outband=1, outline=21, outsample=21,
+                               priority='ontop'),
+                          call(c5.path, mosaic='dummy/mosaic.cub',
+                               outband=1, outline=-9, outsample=-9,
+                               priority='beneath'),
+                          call(c6.path, mosaic='dummy/mosaic.cub',
+                               outband=1, outline=-19, outsample=-19,
+                               priority='beneath')])
+
+    # @patch('PyRISE.HiNoProj.isis.getkey_k', side_effect=getkey)
+    # def test_fix_kernel(self, mock_getkey):
+    #     c5 = hnp.Cube('dummy/PSP_010502_2090_RED5.HiStitch.balance.cub')
+    #     hnp.fix_kernel(c5)
+
+    @patch('PyRISE.HiNoProj.isis.editlab')
+    @patch('PyRISE.HiNoProj.handmos_side')
+    @patch('PyRISE.HiNoProj.shutil.copyfile')
+    @patch('PyRISE.HiNoProj.open', mock_open(read_data='''
+# Average Line Offset: 5
+# Average Sample Offset: 5'''))
+    @patch('PyRISE.HiNoProj.isis.hijitreg')
+    @patch('PyRISE.HiNoProj.isis.noproj')
+    @patch('PyRISE.HiNoProj.isis.spicefit')
+    @patch('PyRISE.HiNoProj.isis.spiceinit')
+    @patch('PyRISE.HiNoProj.isis.getkey_k', side_effect=getkey)
+    def test_HiNoProj(self, mock_getkey, m_spice, m_sfit, m_noproj, m_hijitreg,
+                      m_copy, m_handside, m_edit):
+        c2 = hnp.Cube('dummy/PSP_010502_2090_RED2.HiStitch.balance.cub')
+        c3 = hnp.Cube('dummy/PSP_010502_2090_RED3.HiStitch.balance.cub')
+        c4 = hnp.Cube('dummy/PSP_010502_2090_RED4.HiStitch.balance.cub')
+        c5 = hnp.Cube('dummy/PSP_010502_2090_RED5.HiStitch.balance.cub')
+        c6 = hnp.Cube('dummy/PSP_010502_2090_RED6.HiStitch.balance.cub')
+
+        cubes = (c2, c3, c4, c5, c6)
+
+        conf = {'Shape': 'SYSTEM'}
+
+        hnp.HiNoProj(cubes, c4, 'dummy/mosaic.cub', conf, keep=True)
+        self.assertEqual(len(cubes), m_spice.call_count)
+        self.assertEqual(len(cubes), m_sfit.call_count)
+        self.assertEqual(len(cubes), m_noproj.call_count)
+        self.assertEqual(len(cubes) - 1, m_hijitreg.call_count)
+        self.assertEqual(len(cubes) + 1, m_copy.call_count)
+        self.assertEqual(2, m_handside.call_count)
+        self.assertEqual(m_edit.call_args_list,
+                         [call(Path('dummy/mosaic.cub'), grpname='Instrument',
+                               keyword='ImageJitterCorrected', option='addkey',
+                               value=0),
+                          call(Path('dummy/mosaic.cub'), grpname='Archive',
+                               keyword='ProductId', option='modkey',
+                               value='PSP_010502_2090_RED'),
+                          call(Path('dummy/mosaic.cub'), grpname='Instrument',
+                               keyword='MatchedCube', option='modkey',
+                               value='PSP_010502_2090_RED4'),
+                          call(Path('dummy/mosaic.cub'), grpname='Archive',
+                               keyword='SourceProductId', option='ADDKEY',
+                               value='(PSP_010502_2090_RED4_0, '
+                               'PSP_010502_2090_RED4_1, PSP_010502_2090_RED4_0, '
+                               'PSP_010502_2090_RED4_1, PSP_010502_2090_RED4_0, '
+                               'PSP_010502_2090_RED4_1, PSP_010502_2090_RED4_0, '
+                               'PSP_010502_2090_RED4_1, PSP_010502_2090_RED4_0, '
+                               'PSP_010502_2090_RED4_1)')])
