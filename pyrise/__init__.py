d3e7b243e9e2baad9d5e3db747eade8138b8dc65
Ross Beyer <rbeyer@rossbeyer.net>
2020-05-16 10:15:21 -0700

    Bump version: 0.2.0 → 0.3.0

--- pyrise/__init__.py
+++ pyrise/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Ross A. Beyer"""
 __email__ = 'rbeyer@seti.org'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     packages=find_packages(include=['pyrise', 'pyrise.*']),
     test_suite='tests',
     url='https://github.com/rbeyer/pyrise',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
