31c9eda5d717cf7f67b17d052f1c71fe1eddec5a
Ross Beyer <rbeyer@rossbeyer.net>
2021-09-11 09:42:44 -0700

    docs(HiCal): To match upstream HiCal 1.61 and HiCal.conf 1.41

--- hiproc/HiCal.py
+++ hiproc/HiCal.py
@@ -80,9 +80,9 @@ Output Products:
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# This program is based on HiCal version 4.1.1 (2021/05/03),
-# and on the Perl HiCal program ($Revision: 1.58 $
-#                                $Date: 2021/05/03 22:33:55 $)
+# This program is based on HiCal version 4.2.0 (2021/09/08),
+# and on the Perl HiCal program ($Revision: 1.61 $
+#                                $Date: 2021/09/08 20:23:23 $
 # and on the Perl NoiseFilter program ($Revision: 1.19
 #                                      $Date: 2021/04/22 22:38:39 $)
 # by Eric Eliason and Richard Leis as employees of the University of Arizona.
